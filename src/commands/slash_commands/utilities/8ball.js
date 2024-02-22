7825d38b3266c40b216ae90478475abc3b150623
Alexander Lurita <alexander.lurita.dev@gmail.com>
2024-02-22 15:26:20 -0500

    feat: added 8ball command

---
+++ src/commands/slash_commands/utilities/8ball.js
@@ -0,0 +1,32 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+const { getRandom } = require('../../../utils/getRandom')
+const { eightBallResponses } = require('../../../constants/responses')
+const { colors } = require('../../../constants/colors')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('8ball')
+    .setDescription("Ask the PAL lobby's magic ball")
+    .setDMPermission(false)
+    .addStringOption((option) =>
+      option
+        .setName('question')
+        .setDescription('The question you want to ask')
+        .setRequired(true),
+    ),
+  async execute(interaction, client) {
+    const question = interaction.options.getString('question')
+
+    const anwser = getRandom(eightBallResponses)
+
+    const embedBuilder = new EmbedBuilder()
+      .setColor(colors.warning)
+      .setDescription(`Bola mágica de ${client.user.username}`)
+      .addFields(
+        { name: 'Pregunta', value: question },
+        { name: 'Respuesta', value: anwser },
+      )
+
+    await interaction.reply({ embeds: [embedBuilder] })
+  },
+}
---
+++ src/constants/responses.js
@@ -0,0 +1,19 @@
+const eightBallResponses = [
+  'Sí',
+  'No',
+  'Es probable',
+  'No puedo predecirlo ahora',
+  'Mejor no decirte ahora',
+  'Concéntrate y pregunta otra vez',
+  'No cuentes con ello',
+  'Mis fuentes dicen que no',
+  'Las perspectivas no son buenas',
+  'Todo apunta a que sí',
+  'Definitivamente sí',
+  'Es dudoso',
+  'Sí, definitivamente',
+  'No puedo revelarlo ahora',
+  'Pregunta de nuevo más tarde',
+]
+
+module.exports = { eightBallResponses }
