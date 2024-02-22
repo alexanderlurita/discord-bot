b93aaa3905730d76777fc397717955d2c520c437
Alexander Lurita <alexander.lurita.dev@gmail.com>
2024-02-22 09:57:21 -0500

    feat: added basic say command

---
+++ src/commands/slash_commands/utilities/say.js
@@ -0,0 +1,48 @@
+const { randomUUID } = require('node:crypto')
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  SlashCommandBuilder,
+} = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('say')
+    .setDescription('Send a message through me')
+    .setDMPermission(false)
+    .addStringOption((option) =>
+      option
+        .setName('content')
+        .setDescription('The content of the message to send')
+        .setRequired(true),
+    ),
+
+  async execute(interaction) {
+    const message = interaction.options.getString('content')
+    const user = interaction.member.nickname ?? interaction.user.globalName
+    let sentMessage
+
+    const actionRowBuilder = new ActionRowBuilder().addComponents([
+      new ButtonBuilder()
+        .setCustomId(randomUUID())
+        .setLabel(`Enviado por ${user}`)
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(true),
+    ])
+
+    try {
+      sentMessage = await interaction.channel.send({
+        content: message,
+        components: [actionRowBuilder],
+      })
+    } catch (err) {
+      console.log(err)
+    }
+
+    await interaction.reply({
+      content: sentMessage ? 'Mensaje enviado' : 'No pude enviar el mensaje',
+      ephemeral: true,
+    })
+  },
+}
