e7846f07648340a1b8db430dcefcf3ef40bf9b1f
Alexander Lurita <alexander.lurita.dev@gmail.com>
2024-04-14 16:04:59 -0500

    feat: improve message handling for deletion of both old and recent messages

--- src/commands/slash_commands/moderation/mod/clear.js
+++ src/commands/slash_commands/moderation/mod/clear.js
@@ -27,38 +27,57 @@ module.exports = {
       })
     }
 
-    const messages = await interaction.channel.messages.fetch({ limit: 99 })
+    const messages = await interaction.channel.messages.fetch({
+      limit: user ? 100 : quantity,
+    })
 
     if (messages.size === 0) {
       return await interaction.editReply({
-        content: 'No hay mensajes en este canal para eliminar',
+        content: 'No hay mensajes en este canal para eliminar.',
       })
     }
 
-    const messagesToDelete = user
+    const filteredMessages = user
       ? messages.filter((msg) => msg.author.id === user.id).first(quantity)
       : messages.first(quantity)
 
-    if (messagesToDelete.length === 0) {
+    if (filteredMessages.length === 0) {
       return await interaction.editReply({
         content: `No se encontraron mensajes para borrar${
-          user ? ' de ese usuario' : ''
+          user ? ' de ese usuario.' : '.'
         }`,
       })
     }
 
+    const oldMessages = []
+    const recentMessages = []
+    filteredMessages.forEach((message) => {
+      if (Date.now() - message.createdTimestamp > 1209600000) {
+        oldMessages.push(message)
+      } else {
+        recentMessages.push(message)
+      }
+    })
+
     try {
-      await interaction.channel.bulkDelete(messagesToDelete, true)
+      if (recentMessages.length > 0) {
+        await interaction.channel.bulkDelete(recentMessages, true)
+      }
+      for (const message of oldMessages) {
+        await message.delete()
+      }
     } catch (err) {
       console.log(err)
       return await interaction.editReply({
-        content: 'Ocurrió un error al intentar borrar mensajes',
+        content: 'Ocurrió un error al intentar borrar mensajes.',
       })
     }
 
     await interaction.editReply({
-      content: `¡He borrado \`${messagesToDelete.length} ${
-        messagesToDelete.length === 1 ? 'mensaje' : 'mensajes'
+      content: `¡He borrado \`${recentMessages.length + oldMessages.length} ${
+        recentMessages.length + oldMessages.length === 1
+          ? 'mensaje'
+          : 'mensajes'
       }\`!`,
     })
   },
